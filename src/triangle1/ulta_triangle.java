a3efa165bb48127a68e6660a995d5db25ee73f53
hp1322 <harshpatel170195@gmail.com>
2022-03-08 21:53:49 +0000

    my triangle

---
+++ src/triangle1/ulta_triangle.java
@@ -0,0 +1,17 @@
+package triangle1;
+
+public class ulta_triangle {
+
+    public static void main (String []args){
+
+        System.out.println("*  *  *  *  *  *  *  *  *");
+        System.out.println("  *  *  *  *  *  *  *   ");
+        System.out.println("    *  *  *  *  *      ");
+        System.out.println("      *  *  *        ");
+        System.out.println("         *          ");
+
+
+    }
+
+
+}
